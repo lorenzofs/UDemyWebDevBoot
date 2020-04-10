3245a963dbbac855890b596130e12ac4db1db905
lorenzofs <lsfoggia1@aol.com>
2020-04-10 16:12:51 -0400

    Commit from i 4:12

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,9 +3,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -18,15 +20,7 @@ app.use(express.static("public"))
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {
     listTitle: day,
@@ -36,7 +30,7 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
@@ -58,8 +52,12 @@ app.get("/work", function(req, res) {
   });
 });
 
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
 app.post("/work", function(req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
